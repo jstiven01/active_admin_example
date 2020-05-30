340c093de433478eaf70e0bae0728a4fbcbecf21
Johan Tinjaca <stiven10@gmail.com>
2020-05-30 14:52:39 -0500

    Adding param permit ActiveAdmn User Post

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Post do
+  permit_params :title, :body, :published_at, :user_id
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
