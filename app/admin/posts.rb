7e214bfa305736a3b23a1f710d7b28c2c115e5d3
Johan Tinjaca <stiven10@gmail.com>
2020-05-31 07:23:32 -0500

    Creating publish action in Post admin

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,5 +1,14 @@
 ActiveAdmin.register Post do
   permit_params :title, :body, :published_at, :user_id
+  
+  action_item :view, only: :show do
+    link_to 'Publish', publish_admin_post_path(post), method: :put unless post.published_at?
+  end
+  member_action :publish, method: :put do
+    post = Post.find(params[:id])
+    post.update(published_at: Time.zone.now)
+    redirect_to admin_post_path(post)
+  end
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
